b1b47a74129d77047599de2895d5fba2f3314d1e
Pavanmalegave26 <104684372+Pavanmalegave26@users.noreply.github.com>
2022-04-30 20:36:13 +0530

    File handling
    it Will ne useful FOR coding task

--- bello.c
+++ bello.c
@@ -1,3 +1,10 @@
-#include<lost strem.h>
-#inculde studio.h>
-printf("hello");
+# include <studio.h>
+# include <studio.h
+Main() 
+{
+Declare the file pointer
+Open -Read, write, and Append
+Write content to the file 
+Append content to the file 
+Close the pointe
+}
